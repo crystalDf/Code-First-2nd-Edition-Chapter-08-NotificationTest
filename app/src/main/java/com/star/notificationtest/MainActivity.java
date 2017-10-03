fd11abf4e43f195f55c59a3bcae4c07bda7e454a
crystalDf <chendong333@gmail.com>
2017-10-03 12:50:42 +0800

    Chapter 08 NotificationTest

--- app/src/main/java/com/star/notificationtest/MainActivity.java
+++ app/src/main/java/com/star/notificationtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.star.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         sendNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                PendingIntent pendingIntent =
+                        PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
                 NotificationManager notificationManager =
                         (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
@@ -32,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(
                                 getResources(), R.mipmap.ic_launcher))
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
+                        .setSound(Uri.fromFile(
+                                new File("/system/media/audio/ringtones/Luna.ogg")))
+                        .setVibrate(new long[] {0, 1000, 1000, 1000})
+                        .setLights(Color.GREEN, 1000, 1000)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                                "Learn how to build notifications, " +
+                                        "send and sync data, and use voice actions. " +
+                                        "Get the official Android IDE and developer tools " +
+                                        "to build apps for Android."
+                        ))
                         .build();
 
                 notificationManager.notify(NOTIFICATION_ID, notification);
